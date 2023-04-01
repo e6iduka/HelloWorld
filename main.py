d2fd7ce5802259fb7496c455b7429c714ac15863
egi <egidukaduka27@gmail.com>
2023-04-01 09:54:35 +0200

    exercise with loop

--- main.py
+++ main.py
@@ -1,15 +1,63 @@
-# 1. TASK: print "Hello World"
-print("Hello world")
-# 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
-print("Hello",name)	# with a comma
-print("Hello" + name)	# with a +
-# 3. print "Hello 42!" with the number in a variable
-name = 42
-print("Hello", name)	# with a comma
-print("Hello" + str(name))	# with a +	-- this one should give us an error!
-# 4. print "I love to eat sushi and pizza." with the foods in variables
-fave_food1 = "sushi"
-fave_food2 = "pizza"
-print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
-print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+for x in range(0 ,151):
+    print(x)
+
+
+
+
+for x in range(5 ,1001 ,5):
+    print(x)
+
+
+
+def counting_dojo():
+    for x in range (1,101):
+        if x % 5 == 0:
+            print('Coding')
+        if x % 10 == 0:
+            print ('Dojo')
+        else:
+             print(x)
+
+counting_dojo()
+
+
+
+def count_down_four():
+    number = 2018
+    while number > 0:
+        print (number)
+        number = number - 4
+        
+count_down_four()   
+
+
+
+minimum = 0
+maximum = 500000
+Oddtotal = 0
+
+for number in range(minimum, maximum+1):
+    if(number % 2 != 0):
+        print("{0}".format(number))
+        Oddtotal = Oddtotal + number
+
+print("The Sum of Odd Numbers from {0} to {1} = {2}".format(minimum, maximum, Oddtotal))
+
+
+
+
+def count_down_four():
+    number = 2018
+    while number > 0:
+        print (number)
+        number = number - 4
+        
+count_down_four()   
+
+
+def flex_countdown(low, high, mult):
+    for i in range (low, high):
+        if i % mult == 0:
+            print (i)
+            
+flex_countdown(2, 9, 3)
